678b95b068898188b3e9bf95811fc05e7ab61bdf
tomasadamsky <tomas.adamsky@gmail.com>
2025-01-08 14:13:33 +0100

    first new pc commitment

---
+++ src/app/gdpr/page.tsx
@@ -0,0 +1,13 @@
+
+// C:\Users\AdamskýTomáš\Desktop\snap_zoska-main\src\app\gdpr
+
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box"
+
+export default function TermsConditions() {
+    return (
+        <Box>
+            <Typography> Podmienky používania </Typography>
+        </Box>
+    );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import NavBar from "../components/NavBar";
+
+
+export const metadata: Metadata = {
+  title: "SnapZoška",
+  description: "Vytvorené Tomasom Adamskym",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="sk">
+      <body>
+        <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+          <main style={{ flexGrow: 1 }}>
+            {children}
+          </main>
+          <NavBar /> 
+        </div>
+      </body>
+    </html>
+  );
+}
